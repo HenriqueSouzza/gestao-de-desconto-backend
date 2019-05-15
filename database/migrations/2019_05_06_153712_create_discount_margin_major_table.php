4d395ad8e2c36fd2141c9e6f98a97184eaf1a854
Caio Oliveira <caio.oliveira@cnec.br>
2019-05-15 11:04:07 -0300

    Adicionado campos de codperlet e idperlet no discount

--- database/migrations/2019_05_06_153712_create_discount_margin_major_table.php
+++ database/migrations/2019_05_06_153712_create_discount_margin_major_table.php
@@ -15,9 +15,12 @@ class CreateDiscountMarginMajorTable extends Migration
     {
         Schema::create('discount_margin_major', function (Blueprint $table) {
             $table->bigIncrements('id_discount_margin_major')->autoIncrement();
-            $table->string('id_rm_establishment_discount_margin_major', 10)->comment('ID da unidade do RM tabela GFILIAL RM TOTVS');
-            $table->string('id_rm_major_discount_margin_major', 10)->comment('ID do curso do RM tabela SCURSO RM TOTVS');
+            $table->string('id_rm_establishment_discount_margin_major', 10)->comment('ID da unidade do RM tabela GFILIAL RM TOTVS, CAMPO: CODFILIAL');
+            $table->string('id_rm_major_discount_margin_major', 10)->comment('ID do curso do RM tabela SCURSO RM TOTVS, CAMPO: CODCURSO');
+            $table->integer('id_rm_period_discount_margin_major')->comment('ID do periodo letivo do RM , CAMPO: IDPERLET');
+            $table->string('id_rm_period_code_discount_margin_major', 10)->comment('ID do codigo do periodo do RM, CAMPO: CODPERLET');
             $table->integer('value_discount_margin_major')->comment('Valor do desconto por curso');
+            $table->integer('fk_user_margin_major')->comment('ID do usuario que criou a insercao');
             $table->timestamp('created_at_discount_margin_major')->useCurrent();
             $table->timestamp('updated_at_discount_margin_major')->nullable();
             $table->timestamp('deleted_at_discount_margin_major')->nullable();
