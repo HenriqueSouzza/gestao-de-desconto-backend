420c1abe8b5bb53e2b9e00c90de827a0e599ee17
Caio Oliveira <caio.oliveira@cnec.br>
2019-05-20 10:28:36 -0300

    Insercao do concession period

--- database/seeds/ConcessionPeriodsSeeder.php
+++ database/seeds/ConcessionPeriodsSeeder.php
@@ -20,5 +20,14 @@ class ConcessionPeriodsSeeder extends Seeder
             'date_end_concession_period' => '2019-06-06',
             'fk_user' => 1
         ]);
+        DB::table('concession_periods')->insert([
+            'id_rm_establishment_concession_period'=> 169,
+            'id_rm_modality_concession_period' => 'D',
+            'id_rm_period_concession_period' => 2396,
+            'id_rm_period_code_concession_period' => '2019-12',
+            'date_start_concession_period' => '2019-01-01',
+            'date_end_concession_period' => '2019-06-06',
+            'fk_user' => 1
+        ]);
     }
 }
