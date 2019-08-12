a48b32a4ceb583d9e8836b275aaa4aaffdd4ff98
Caio Oliveira <caio.oliveira@cnec.br>
2019-08-12 15:35:57 -0300

    COd servico da pos

--- app/Http/Controllers/Api/StudentSchoolarshipController.php
+++ app/Http/Controllers/Api/StudentSchoolarshipController.php
@@ -617,6 +617,23 @@ class StudentSchoolarShipController extends Controller
                 return $codBolsa;
         }
     }
+
+    /**
+     * Pegar codservico
+     */
+    private function getCodServico($i, $course_type){
+        if($course_type == 3){ // GRAD
+            if($i == 1)
+                return 1;
+            return 2;
+        }
+        else if($course_type == 4) {// POS
+            if($i == 1)
+                return 6;
+            return 7;
+        }
+    }
+
     /**
      * @param Request $request (com os dados acima no formato JSON)
      * @return array $requestSoap     * 
@@ -677,8 +694,9 @@ class StudentSchoolarShipController extends Controller
                 if ($discount->send_rm) {   //caso passe o idbolsaaluno o registro irá ser atualizado se não sera criado
                     $studentSchoolarship = (isset($discount->student_schoolarship) ? $discount->student_schoolarship : 'xsi');
                     $dataServer = 'EduBolsaAlunoData';
+                    
                     for ($i = $discount->first_installment; $i <= $discount->last_installment; $i++) {
-
+                        $codServico = getCodServico($i, $discount->course_type);
                         $xmlRequest = [
                             'SBolsaAluno' => [
                                 ['IDBOLSAALUNO'   => $studentSchoolarship],
@@ -689,7 +707,7 @@ class StudentSchoolarShipController extends Controller
                                 ['IDPERLET'       => $discount->period],
                                 ['CODCONTRATO'    => $discount->contract],
                                 ['CODBOLSA'       => $i == 1 ? $this->codBolsaP1($discount->schoolarship) : $discount->schoolarship],
-                                ['CODSERVICO'     => $i == 1 ? 1 : 2],
+                                ['CODSERVICO'     => $codServico],
                                 ['DESCONTO'       => $discount->value],
                                 ['TIPODESC'       => 'P'],
                                 ['CODUSUARIO'     => 'wsgestaodedesconto'],
